3eb7015550b98fbf054e39769c71e47dc7a0f72a
David Nguyen <david.nguyen@smartsheet.com>
2018-02-17 17:12:34 -0800

    updates the form

---
+++ webform.js
@@ -0,0 +1,8 @@
+//testing out of the script is picked up by HTML page
+console.log("script implemented into the page")
+
+//submit function to get email
+function signUpForm(){
+    event.preventDefault();
+    console.log("form submitted");    
+}
