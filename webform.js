37470feaf010b991a2cd055a46c9fde4bf28681c
David Nguyen <david.nguyen@smartsheet.com>
2018-02-18 22:23:28 -0800

    updates the page

--- webform.js
+++ webform.js
@@ -4,5 +4,7 @@ console.log("script implemented into the page")
 //submit function to get email
 function signUpForm(){
     event.preventDefault();
+    var firstName = document.emailForm.firstName.value;
+    
     console.log("form submitted");    
 }
