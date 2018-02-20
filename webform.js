89dec5007210d585a23bd20d339c01e4f4f1d7f6
davednguyen <davednguyen@gmail.com>
2018-02-20 14:15:04 -0800

    update more function to submit form

--- webform.js
+++ webform.js
@@ -2,7 +2,7 @@
 console.log("script implemented into the page")
 
 //submit function to get email
-function signUpForm(){
+function signUpForm() {
     event.preventDefault();
 
     //get values from the form
@@ -10,7 +10,7 @@ function signUpForm(){
     var lastName = document.custSignUpForm.lName.value;
     var emailAddress = document.custSignUpForm.email.value;
     var phoneNumber = document.custSignUpForm.phone.value;
-    var confirmNote = "Thanks "+ firstName + " ,you are on our email list!              ";
+    var confirmNote = "Thanks " + firstName + " ,you are on our email list!";
 
     //log the info get from form
     console.log(confirmNote);
@@ -29,4 +29,23 @@ function signUpForm(){
     customerPhone.innerHTML = "Phone number: " + phoneNumber;
     var customerEmail = document.getElementById("infoLine3");
     customerEmail.innerHTML = "Email address: " + emailAddress;
+    var modifySection = document.getElementById("modify");
+    modifySection.style.display = "block";
+    document.getElementById("emailForm").reset();
+};
+
+//function to remove customer from email list per his/her request
+function removeInfo() {
+    //display info on the page
+    var confirmNote = "Hello ,your info removed from our email list!";
+    var confirmInfo = document.getElementById("confirmNote");
+    confirmInfo.innerHTML = confirmNote;
+    var fullName = document.getElementById("infoLine1");
+    fullName.innerHTML = "";
+    var customerPhone = document.getElementById("infoLine2");
+    customerPhone.innerHTML = "";
+    var customerEmail = document.getElementById("infoLine3");
+    customerEmail.innerHTML = "";
+    var modifySection = document.getElementById("modify");
+    modifySection.style.display = "none";
 }
