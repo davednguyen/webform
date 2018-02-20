7720e652ddde305e83d0f5dff5a37d8153a6061e
David Nguyen <david.nguyen@smartsheet.com>
2018-02-19 19:37:04 -0800

    update the form field

--- webform.js
+++ webform.js
@@ -4,7 +4,13 @@ console.log("script implemented into the page")
 //submit function to get email
 function signUpForm(){
     event.preventDefault();
-    var firstName = document.emailForm.firstName.value;
-    
+    var firstName = document.custSignUpForm.fName.value;
+    var lastName = document.custSignUpForm.lName.value;
+    var emailAddress = document.custSignUpForm.email.value;
+    var phoneNumber = document.custSignUpForm.phone.value;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(emailAddress);
+    console.log(phoneNumber);
     console.log("form submitted");    
 }
